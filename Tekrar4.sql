1c152e83b0ab6627ddfc2fe6daf45775dab42593
ercancelikbusiness <ercancelikbusiness@gmail.com>
2025-11-18 21:19:25 +0300

    .

--- Tekrar4.sql
+++ Tekrar4.sql
@@ -20,9 +20,23 @@ set @toplamMaas=225000.68
 
 --print 'personel Sayisi:'+ convert(varchar(10),@personelsayisi)
 --yada
---print 'personel SAyisi:' + cast(@personelsayisi as varchar(10)) -- varcharlardan sonra parantezle sayý belirtmesekde olur
+--print 'personel SAyisi:' + cast(@personelsayisi as varchar(10)) -- varcharlardan sonra parantezle sayý belirtmesekde olur 
+--ama  cast kullanýyorsak  varchardan önce as kullanmalýyýz convertte ise direkt (varchar , @...) yapýlabilir
+--****** ayrýca @ li deðiþkenlerdeki büyük küçük harf birebir olmasýna gerek yok @PersonelSayisi yazarsan yinede oluyor
 
-print @personelSayisi  -- bu þekilde düz yazýlabilir  
+--print 'Personel Sayisi::::' + convert(varchar,@personelsayisi)
+--print 'Personel Sayisi::::' + cast(@personelsayisi as varchar)
+
+--print 'Personel Sayisi::::' + cast(@personelsayisi as varchar)+' toplaMMaas:::'+cast(@toplammaas as varchar)
+
+--print @personelSayisi  -- bu þekilde düz yazýlabilir  
+
+set @personelsayisi =(select count(*) from BikeStoresTekrar.sales.staffs) -- burdaki dbo deðil
+set @toplamMaas=(select sum(Personel.Maas) from BeltekTekrarDB.dbo.Personel) 
+
+--not: sum(personel.maas)  yerine (maas) yazýlabilirdi bu arada personel tablonun ismi kýsaltma gibi kullanýldý çünkü tablonun ismi bu
+
+print 'ToplamMaas: '+ cast(@toplammaas as varchar) 
 
 
 
